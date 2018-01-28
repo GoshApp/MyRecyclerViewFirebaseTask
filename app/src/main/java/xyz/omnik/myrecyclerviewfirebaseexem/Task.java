da7f547d7a29eabb2ac6e206b19fa5a9fd89924b
Rikmen <rikmenv@yandex.ru>
2018-01-28 14:31:37 +0300

    Initial commit

---
+++ app/src/main/java/xyz/omnik/myrecyclerviewfirebaseexem/Task.java
@@ -0,0 +1,16 @@
+package xyz.omnik.myrecyclerviewfirebaseexem;
+
+/**
+ * Created by Rikmen on 28.01.2018.
+ */
+
+public class Task {
+    private String task;
+    public Task() {}
+    public Task(String task) {
+        this.task = task;
+    }
+    public String getTask() {
+        return task;
+    }
+}
